b4ad20bf743439182efcff33c1ded3a4195b0f3d
TheGuyWithoutH <ugo.balducci@epfl.ch>
2022-11-05 13:39:25 +0000

    Sharing Old Version

--- A2/sharing.c
+++ A2/sharing.c
@@ -34,25 +34,26 @@ int main (int argc, const char *argv[]) {
 int perform_buckets_computation(int num_threads, int num_samples, int num_buckets) {
     volatile int *histogram = (int*) calloc(num_buckets, sizeof(int));
     int** tmp_hist = calloc(num_threads, sizeof(histogram));
+    int val, tid;
 
     omp_set_num_threads(num_threads);
 
-    #pragma omp parallel private(num_samples, num_buckets) shared(tmp_hist)
+    #pragma omp parallel private(val, tid) shared(tmp_hist)
     {
-        int tid = omp_get_thread_num();
+        tid = omp_get_thread_num();
         rand_gen generator = init_rand();
         tmp_hist[tid] = (int*)calloc(num_buckets, sizeof(int));
 
         #pragma omp for nowait
         for(int i = 0; i < num_samples; i++){
-            int val = next_rand(generator) * num_buckets;
+            val = next_rand(generator) * num_buckets;
             tmp_hist[tid][val]++;
         }
 
         free_rand(generator);
     }
 
-    #pragma omp parallel shared (tmp_hist, histogram) private(num_buckets)
+    #pragma omp parallel for shared (tmp_hist, histogram) private(num_buckets)
     for(int i = 0; i < num_buckets; i++) {
         int tid;
         for (tid = 0; tid < omp_get_num_threads(); tid++) {
