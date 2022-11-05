6dd17f0cd05e75cfa7abdc4db6084679819bc9fc
Ahmed Kamal Elalamy <ahmed.elalamy@epfl.ch>
2022-11-05 13:41:55 +0100

    part 2 about to give up

--- A2/algorithm.c
+++ A2/algorithm.c
@@ -6,7 +6,6 @@ SCIPER      : 324610
 
 ============================================================================
 */
-#include <math.h>
 
 #define INPUT(I,J) input[(I)*length+(J)]
 #define OUTPUT(I,J) output[(I)*length+(J)]
@@ -16,30 +15,34 @@ void simulate(double *input, double *output, int threads, int length, int iterat
     double *temp;
     omp_set_num_threads(threads);
     // Parallelize this!!
-    int n, i, j, tid;
-    double tmp_in[threads][length*length];
-    double tmp_out[threads][length*length];
-#pragma omp parallel private(n,i,j, tid) shared(tmp_in, tmp_out)
+    int n, i, j;
+#pragma omp parallel private(n,i,j)
     {
-        tid = omp_get_thread_num();
-        init(tmp_in[tid], length);
-        init(tmp_out[tid], length);
-        for (n = 0; n < iterations/threads; n++) {
-            for (i = 1; i < length - 1; i++) {
-                for (j = 1; j < length - 1; j++) {
-                    if (((i == length / 2 - 1) || (i == length / 2))
-                        && ((j == length / 2 - 1) || (j == length / 2)))
-                        continue;
+    for (n = 0; n < iterations; n++) {
+    #pragma omp for
+        for (i = 1; i < length - 1 ; i++) {
+            for (j = 1; j < length - 1; j+=2) {
+                if (((i == length / 2 - 1) || (i == length / 2))
+                    && (j == length / 2 - 1))
+                    continue;
 
-                    OUTPUT(i, j) = (INPUT(i - 1, j - 1) + INPUT(i - 1, j) + INPUT(i - 1, j + 1) +
-                                    INPUT(i, j - 1) + INPUT(i, j) + INPUT(i, j + 1) +
-                                    INPUT(i + 1, j - 1) + INPUT(i + 1, j) + INPUT(i + 1, j + 1)) / 9;
-                }
+
+                OUTPUT(i, j) = (INPUT(i - 1, j - 1) + INPUT(i - 1, j) + INPUT(i - 1, j + 1) +
+                                INPUT(i, j - 1) + INPUT(i, j) + INPUT(i, j + 1) +
+                                INPUT(i + 1, j - 1) + INPUT(i + 1, j) + INPUT(i + 1, j + 1)) / 9;
+
+                 OUTPUT(i, j + 1) = (INPUT(i - 1, j) + INPUT(i - 1, j + 1) + INPUT(i - 1, j + 2) +
+                                 INPUT(i, j) + INPUT(i, j + 1) + INPUT(i, j + 2) +
+                                 INPUT(i + 1, j) + INPUT(i + 1, j + 1) + INPUT(i + 1, j + 2)) / 9;
+
+
             }
-
-            temp = input;
-            input = output;
-            output = temp;
         }
+
+        temp = input;
+        input = output;
+        output = temp;
+    }
     }
 }
+
--- A2/execute.sh
+++ A2/execute.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-#SBATCH --chdir /scratch/<username>
+#SBATCH --chdir /home/aelalamy/mularch-fall-22/A2 
 #SBATCH --nodes 1
 #SBATCH --ntasks 1
-#SBATCH --cpus-per-task 16
+#SBATCH --cpus-per-task 36
 #SBATCH --mem 2G
 
 
 echo STARTING AT `date`
 
-./sharing 1 100000000 32
-
-# ./heatmap 1 10000 100 output.csv
+./heatmap 1 10000 100 output1.csv
+./heatmap 2 10000 100 output2.csv
+./heatmap 4 10000 100 output4.csv
+./heatmap 8 10000 100 output8.csv
+./heatmap 16 10000 100 output16.csv
 
 echo FINISHED at `date`
