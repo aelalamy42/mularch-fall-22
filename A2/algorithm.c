af6198df4af076459132edac30121968f7a04486
Ahmed Kamal Elalamy <ahmed.elalamy@epfl.ch>
2022-11-06 19:36:13 +0100

    SUBMIT

--- A2/algorithm.c
+++ A2/algorithm.c
@@ -21,20 +21,17 @@ void simulate(double *input, double *output, int threads, int length, int iterat
     for (n = 0; n < iterations; n++) {
     #pragma omp for
         for (i = 1; i < length - 1 ; i++) {
-            for (j = 1; j < length - 1; j+=2) {
-                if (((i == length / 2 - 1) || (i == length / 2))
-                    && (j == length / 2 - 1))
-                    continue;
+            for(int j=1; j<length-1; j+=8)
+            {
+                for(int k = j; k < j + 8 && k < length - 1; k++) {
+                    if ( ((i == length/2-1) || (i== length/2))
+                         && ((k == length/2-1) || (k == length/2)) )
+                        continue;
 
-
-                OUTPUT(i, j) = (INPUT(i - 1, j - 1) + INPUT(i - 1, j) + INPUT(i - 1, j + 1) +
-                                INPUT(i, j - 1) + INPUT(i, j) + INPUT(i, j + 1) +
-                                INPUT(i + 1, j - 1) + INPUT(i + 1, j) + INPUT(i + 1, j + 1)) / 9;
-
-                 OUTPUT(i, j + 1) = (INPUT(i - 1, j) + INPUT(i - 1, j + 1) + INPUT(i - 1, j + 2) +
-                                 INPUT(i, j) + INPUT(i, j + 1) + INPUT(i, j + 2) +
-                                 INPUT(i + 1, j) + INPUT(i + 1, j + 1) + INPUT(i + 1, j + 2)) / 9;
-
+                    OUTPUT(i,k) = (INPUT(i-1,k-1) + INPUT(i-1,k) + INPUT(i-1,k+1) +
+                                   INPUT(i,k-1)   + INPUT(i,k)   + INPUT(i,k+1)   +
+                                   INPUT(i+1,k-1) + INPUT(i+1,k) + INPUT(i+1,k+1) )/9;
+                }
 
             }
         }
@@ -45,4 +42,3 @@ void simulate(double *input, double *output, int threads, int length, int iterat
     }
     }
 }
-
